e750a8057ddb67440affb5c0e5343f614b3bf7ad
AMP <52510403+ArMaP-i@users.noreply.github.com>
2025-07-22 19:48:42 -0300

    Add files via upload

---
+++ prova.py
@@ -0,0 +1,10 @@
+number = int(input('escreva seu numero = '))
+
+
+
+if number > 0:
+    print(f'seu numero, {number}, é Positivo')
+elif number < 0:
+   print(f'seu numero, {number}, é Negativo')
+elif number == 0:
+    print('seu numero é zero')
